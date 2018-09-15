62978126f17a52f01f2670a65a6fe6fa3f616b02
lavo <lavo@outlook.com>
2018-09-14 21:06:07 -0300

    criando pageadmin e tpl admin

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \Hcode\Page;
+use \Hcode\PageAdmin;
 
 $app = new Slim();
 
@@ -12,6 +13,14 @@ $app->config('debug', true);
 $app->get('/', function() {      //Quando chamarem o site da pasta raiz...
     
 	$page = new Page();  //Chama o construct e adiciona o header
+
+	$page->setTpl("index");  // Carrge o conteúdo 
+	//O destrutor vai mostrar o footer
+});
+
+$app->get('/admin', function() {      //Quando chamarem o site da pasta raiz...
+    
+	$page = new PageAdmin();  //Chama o construct e adiciona o header
 
 	$page->setTpl("index");  // Carrge o conteúdo 
 	//O destrutor vai mostrar o footer
