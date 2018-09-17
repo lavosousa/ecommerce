80eb9925d97868781ecf09e33499c573defa41be
lavo <lavo@outlook.com>
2018-09-16 23:52:50 -0300

    recuperar senha

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ $app = new Slim();
 
 $app->config('debug', true);
 
-$app->get('/', function() {      //Quando chamarem o site da pasta raiz...
+$app->get('/', function() {      //Quando chamarem o site da pasta raiz(loja)...
     
 	$page = new Page();  //Chama o construct e adiciona o header
 
@@ -19,7 +19,7 @@ $app->get('/', function() {      //Quando chamarem o site da pasta raiz...
 	//O destrutor vai mostrar o footer
 });
 
-$app->get('/admin', function() {     
+$app->get('/admin', function() {    // rota pagina admin 
 
 	User::verifyLogin();
     
@@ -29,7 +29,7 @@ $app->get('/admin', function() {
 	
 });
 
-$app->get('/admin/login', function() {      
+$app->get('/admin/login', function() {      // Login get
     
 	$page = new PageAdmin([
 		"header" => false,
@@ -40,7 +40,7 @@ $app->get('/admin/login', function() {
 	
 });
 
-$app->post('/admin/login', function() {
+$app->post('/admin/login', function() {	   // Login post
     
 	User::login($_POST["login"], $_POST["password"]);
 
@@ -49,7 +49,7 @@ $app->post('/admin/login', function() {
 	
 });
 
-$app->get('/admin/logout', function() {
+$app->get('/admin/logout', function() {		 // Logout get
     
 	User::logout();
 
@@ -57,6 +57,176 @@ $app->get('/admin/logout', function() {
 	exit;   
 	
 });
+
+$app->get('/admin/users', function() {    // rota pagina admin / usuários
+
+	User::verifyLogin();    // ja valida se está logado e se é admin
+
+	$users = User::listAll();  // traz array com todos usuários
+    
+	$page = new PageAdmin();  // carrega pagina padrão com header e footer (normal) 
+
+	$page->setTpl("users", array (
+		"users"=>$users
+	));
+
+	$page->setTpl("users");  
+	
+});
+
+$app->get('/admin/users/create', function() {   
+	User::verifyLogin();    
+    
+	$page = new PageAdmin();  
+
+	$page->setTpl("users-create");  	
+});
+
+$app->get('/admin/users/:iduser/delete', function($iduser) {    // tem que vir antes pois se vir depois do prox nunca será executado.
+
+	User::verifyLogin();
+
+	$user = new User(); // Carregar o usuário pra saber que ainda exite no bd;
+
+	$user->get((int)$iduser);
+
+	$user->delete();
+
+	header("Location: /admin/users");
+	exit;
+
+});
+
+$app->get('/admin/users/:iduser', function($iduser) {    // rota pagina admin / usuários
+
+	User::verifyLogin();
+
+	$user = new User();
+
+	$user->get((int)$iduser);
+    
+	$page = new PageAdmin();  
+
+	$page->setTpl("users-update", array(
+		"user"=>$user->getValues()
+	));  
+	
+});
+
+$app->post('/admin/users/create', function() {      // create via post para gravar
+
+	User::verifyLogin();
+
+	$user = new User();
+
+	$_POST["inadmin"] = (isset($_POST["inadmin"]))?1:0;  
+
+ 	$_POST['despassword'] = password_hash($_POST["despassword"], PASSWORD_DEFAULT, [
+ 		"cost"=>12 
+ 	]);
+
+	$user->setData($_POST);
+		   
+	$user->save();
+
+	header("Location: /admin/users");	
+	exit;
+	
+});
+
+$app->post('/admin/users/:iduser', function($iduser) {    // create via post para gravar, agora recebendo
+
+	User::verifyLogin();   
+
+	$user = new User();
+
+	$_POST["inadmin"] = (isset($_POST["inadmin"]))?1:0;
+
+	$user->get((int)$iduser);
+
+	$user->setData($_POST);
+
+	$user->update();
+
+	header("Location: /admin/users");	
+	exit;
+
+});
+
+$app->get('/admin/forgot', function() {    
+
+	$page = new PageAdmin([
+		"header" => false,
+		"footer" => false
+	]);
+
+	$page->setTpl("forgot");
+});
+
+
+$app->post('/admin/forgot', function() {    
+
+	$user = User::getForgot($_POST["email"]);
+
+	header("Location: /admin/forgot/sent");	
+	exit;
+
+});
+
+$app->get('/admin/forgot/sent', function() {    
+
+	$page = new PageAdmin([
+		"header" => false,
+		"footer" => false
+	]);
+
+	$page->setTpl("forgot-sent");
+
+});
+
+$app->get('/admin/forgot/reset', function() {
+
+	$user = User::validForgotDecrypt($_GET["code"]);   // recebe o codigo via get
+
+	$page = new PageAdmin([
+		"header" => false,
+		"footer" => false
+	]);
+
+	$page->setTpl("forgot-reset", array(
+		"name"=>$user["desperson"],
+		"code"=>$_GET["code"]
+	));
+
+});
+
+$app->post('/admin/forgot/reset', function() {
+
+	$forgot = User::validForgotDecrypt($_POST["code"]);   // recebe o codigo via get
+
+	User::setForgotUsed($forgot["idrecovery"]); // PAra invalidar o código apos uso.
+
+	$user = new User();
+
+	$user->get((int)$forgot["iduser"]);
+
+	$password = password_hash($_POST["password"], PASSWORD_DEFAULT, [
+		"cost"=>12
+	]);
+
+	$user->setPassword($password);
+
+	$page = new PageAdmin([
+		"header" => false,
+		"footer" => false
+	]);	
+
+	$page->setTpl("forgot-reset-success");
+
+});
+
+
+
 
 $app->run();
 
--- vendor/hcodebr/php-classes/src/Model.php
+++ vendor/hcodebr/php-classes/src/Model.php
@@ -16,25 +16,29 @@ class Model {
 		{
 
 			case "get":
-				$this->values[$fieldName];   // quando for get ele pega
+				return (isset($this->values[$fieldName])) ? $this->values[$fieldName] : NULL;	
 			break;
 
 			case "set":
-				$this->values[$fieldName] = $args[0];   // quando ser ele atribui
+				$this->values[$fieldName] = $args[0];   // quando ser ele atribui			
 			break;
 
 		}
 
 	}
 
-	public function setData($data = array())
+	public function setData($data = array())   // setar automaticamente
 	{
+
+		//var_dump($data);
 
 		foreach ($data as $key => $value) {
 
-				$this->{"set".$key}($value);
+			$this->{"set".$key}($value);    // criação dinamica do set+o campo. ex: setIduser {"set".$key}  
 
 		}
+
+		//var_dump($this);
 
 	}
 
