be7dfd7c764a498038d0408180fb75c488dd920a
lavo <lavo@outlook.com>
2018-09-20 17:41:37 -0300

    Site categorias

---
+++ views-cache/categories-menu.c5b813be90a10a19f97859c318d75589.rtpl.php
@@ -0,0 +1,1 @@
+<?php if(!class_exists('Rain\Tpl')){exit;}?><li><a href="/categories/5">Android</a></li><li><a href="/categories/1">Google</a></li><li><a href="/categories/13">Motorola</a></li><li><a href="/categories/12">Samsung</a></li>
