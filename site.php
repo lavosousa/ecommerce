6bad8e848832a0b5ce85016720fc0da42646781c
lavo <lavo@outlook.com>
2018-09-25 19:51:22 -0300

    Site produtos

--- site.php
+++ site.php
@@ -1,12 +1,17 @@
 <?php
 
 use \Hcode\Page;
+use \Hcode\Model\Product;
 
 $app->get('/', function() {      //Quando chamarem o site da pasta raiz(loja)...
+
+	$products = Product::listAll();
     
 	$page = new Page();  //Chama o construct e adiciona o header
 
-	$page->setTpl("index");  // Carrge o conteúdo 
+	$page->setTpl("index", [
+		"products"=>Product::checkList($products)
+	]); // Carrge o conteúdo 
 	//O destrutor vai mostrar o footer
 });
 
